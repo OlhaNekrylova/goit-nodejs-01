ad93d1f7522ee91bc58724b74b7064e9ca8ac762
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-03-29 16:17:52 +0300

    change files

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const argv = require("yargs").argv;
 
-const contacts = require("./contacts");
+const {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+} = require("./contacts");
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
     switch (action) {
         case "list":
-            const allContacts = await contacts.listContacts();
+            const allContacts = await listContacts();
             return console.log(allContacts);
         
         case "get":
-            const oneContact = await contacts.getContactById(id);
+            const oneContact = await getContactById(id);
             return console.log(oneContact);
         
         case "add":
-            const newContact = await contacts.addContact({name, email, phone});
+            const newContact = await addContact({name, email, phone});
             return console.log(newContact);
         
         case "remove":
-            const deleteContact = await contacts.removeContact(id);
+            const deleteContact = await removeContact(id);
             return console.log(deleteContact);
         
         default:
